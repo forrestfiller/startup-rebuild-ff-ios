e2404d80e8a21393c2d3d6d78c62a172edeaeb96
Forrest Filler <forrestfiller@users.noreply.github.com>
2016-07-24 17:38:49 -0400

    wired up new startup Vc, tableview, storyboard

--- Startup-rebuild-FF/StartupViewController.swift
+++ Startup-rebuild-FF/StartupViewController.swift
@@ -8,17 +8,35 @@
 
 import UIKit
 
-class StartupViewController: UIViewController {
+class StartupViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet var startupsTable: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 40
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cellId = "cellId"
+        if let cell = tableView.dequeueReusableCellWithIdentifier(cellId){
+            cell.textLabel?.text = "\(indexPath.row)"
+            return cell
+        }
+        
+        let cell = UITableViewCell(style: .Subtitle, reuseIdentifier: cellId)
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
     }
     
 
